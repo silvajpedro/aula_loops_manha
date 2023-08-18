578c2d894c6449de50e8c8bf2b466206e0e470aa
silvajpedro <silvajpro.31@gmail.com>
2023-08-18 10:50:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,130 @@
+// vamos contar de 1 até 100
+
+let contador = 0
+
+console.log(contador + 1)
+console.log(contador + 1)
+console.log(contador + 1)
+console.log(contador + 1)
+console.log(contador + 1)
+
+
+
+// 0 + 1 =1
+// 1 + 1 = 2
+// 2 + 1 = 3
+// 3 + 1 = 4
+// 4 + 1 = 5
+
+// operadores de atribuição!
+// atribuir um valor ou diminuir esse valor
+
+let conta = 0
+
+// atribuição de adição
+// +=
+
+console.log(conta += 1)
+console.log(conta += 1)
+console.log(conta += 1)
+console.log(conta += 1)
+console.log(conta += 1)
+console.log(conta += 1)
+console.log(conta += 1)
+
+
+let precoTotal = 0
+
+
+console.log(precoTotal = precoTotal + 100 )
+
+console.log(precoTotal += 100)
+// 100
+
+console.log(precoTotal += 100)
+// 200
+
+console.log(precoTotal += 100)
+// 300
+
+
+// - atribui uma subtração
+// -=
+console.log(precoTotal -= 100)
+
+console.log(precoTotal -= 50)
+
+// * atribui uma multiplicação
+// *=
+console.log(precoTotal *= 2)
+
+//  atribui uma divisão
+// /=
+
+console.log(precoTotal /= 2)
+
+
+// let segundoContador = 0
+
+// segundoContador = segundoContador + 1
+
+
+// console.log(segundoContador += 1)
+
+// console.log(segundoContador += 1)
+
+// console.log(segundoContador += 1)
+
+// console.log(segundoContador += 1)
+
+// console.log(segundoContador +=1)
+
+// console.log(segundoContador += 1)
+
+// console.log(segundoContador += 1)
+
+// LOOPS
+
+
+// while()
+// enquanto
+
+// while(nossa condição){
+//     faça a ação
+// }
+
+
+// let terceiroContador = 0
+
+// a gente lê enquanto
+ 
+// while(terceiroContador <= 10){
+
+// console.log(` contei até ${terceiroContador}`)
+
+// terceiroContador+=1
+
+// }
+
+
+
+
+// apesar de serem parecidos com o if, o loop executa várias vezes a nossa ação, enquanto o if executa apenas 1 vez.
+
+
+//   para                enquanto          faça
+
+// for(let contador2 = 0; contador2 <= 25; contador2+=1){
+// console.log(contador2)
+// }
+
+// faça  enquanto
+
+
+let contador3 = 21
+do{
+
+    console.log(`O numero ${contador3} e menor que 20`)
+
+} while(contador3 <= 20 )
+
